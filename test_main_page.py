0262e024b51fef9b407f6622381fce64a1190ede
Iska <iskandarmustafin@gmail.com>
2021-10-12 17:27:03 +0300

    almost complete conversion to Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,11 @@
-import time
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, url=link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
